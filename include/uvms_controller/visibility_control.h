2f990eaf67a9239d0fa3957d342db3fa480405d4
edward morgan <edmorgangh@gmail.com>
2024-07-28 14:10:06 -0500

    visibility control.h

--- include/uvms_controller/visibility_control.h
+++ include/uvms_controller/visibility_control.h
@@ -1,4 +1,4 @@
-// Copyright 2020 PAL Robotics S.L.
+// Copyright 2024 Edward Morgan
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
