0f37086e62fa7075cc46aee12518020ebef6c000
edward <papaacquah17@gmail.com>
2024-11-04 15:32:20 -0600

    update

--- src/uvms_base.cpp
+++ src/uvms_base.cpp
@@ -180,8 +180,8 @@ namespace uvms_controller
       uvms.flow_velocity.assign(6, 0.0);
       uvms.model_p = {1e-05, 1e-05, 1e-05, 1e-05, 3.0, 2.3, 2.2, 0.3, 3.0, 1.8, 1.0, 1.15};
 
-      model_dynamics.coupled_simulate(uvms.id);
-      // model_dynamics.decoupled_simulate(uvms.id);
+      // model_dynamics.coupled_simulate(uvms.id);
+      model_dynamics.decoupled_simulate(uvms.id);
 
       set_command_values(uvms.poseCommander, uvms.next_position, 11);
       set_command_values(uvms.velCommander, uvms.next_velocity, 10);
